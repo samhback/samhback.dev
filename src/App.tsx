9d21b9c44bd62285368507dbdee0f6e03333b995
Samuel Holsomback <samuelphbusiness@gmail.com>
2025-04-06 23:58:04 -0500

    please work

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Topbar from './components/Topbar'
 import Contact from './components/Contact'
